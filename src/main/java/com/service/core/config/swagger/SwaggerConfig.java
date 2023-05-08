f4138cda308c8325aa016550519381d155a92061
HwangHarim <scopan01@naver.com>
2023-05-08 15:28:54 +0900

    :tada: init: project setting

--- src/main/java/com/service/core/config/swagger/SwaggerConfig.java
+++ src/main/java/com/service/core/config/swagger/SwaggerConfig.java
@@ -1,49 +1,30 @@
 package com.service.core.config.swagger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableWebMvc
+
+// SwaggerConfig.java
+@OpenAPIDefinition(
+    info = @Info(title = "채팅서비스 API 명세서",
+        description = "DDD를 기반한",
+        version = "v1"))
+@RequiredArgsConstructor
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
-
-    private ApiInfo apiInfo(){
-        return new ApiInfoBuilder()
-            .title("glass bottle Api")
-            .description("swagger 설정")
-            .version("1.0")
-            .build();
-    }
+public class SwaggerConfig {
 
     @Bean
-    public Docket allApi(){
-        return new Docket(DocumentationType.SWAGGER_2)
-            .useDefaultResponseMessages(false)
-            .select()
-            .apis(RequestHandlerSelectors.basePackage("com.service.core"))
-            .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.ant("/api/**"))
-            .build()
-            .apiInfo(apiInfo());
-    }
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/api/v1/**"};
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-            .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        return GroupedOpenApi.builder()
+            .group("bottle API v1")
+            .pathsToMatch(paths)
+            .build();
     }
 }
 
