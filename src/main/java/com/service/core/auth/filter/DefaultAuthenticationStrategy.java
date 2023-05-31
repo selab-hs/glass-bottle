446614d2cb2bc0983926bc762092f4fec530ba57
HwangHarim <scopan01@naver.com>
2023-06-01 03:16:14 +0900

    :sparkles: JWT를 사용한 로그인 구현

---
+++ src/main/java/com/service/core/auth/filter/DefaultAuthenticationStrategy.java
@@ -0,0 +1,20 @@
+package com.service.core.auth.filter;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DefaultAuthenticationStrategy implements GetAuthenticationStrategy, SetAuthenticationStrategy{
+
+    @Override
+    public Authentication get(HttpServletRequest request) {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @Override
+    public void set(Authentication authentication) {
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
