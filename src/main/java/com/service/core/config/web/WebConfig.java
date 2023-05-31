2e0b4a677eaae6054de062c531cf0310ec3ce398
HwangHarim <scopan01@naver.com>
2023-06-01 03:16:58 +0900

    :sparkles: argument resolver 통한 회원 인증 방식 구현

--- src/main/java/com/service/core/config/web/WebConfig.java
+++ src/main/java/com/service/core/config/web/WebConfig.java
@@ -1,17 +1,23 @@
 package com.service.core.config.web;
 
+import com.service.core.auth.presentation.AuthMemberArgumentResolver;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthMemberArgumentResolver authMemberArgumentResolver;
+
     @Bean
     @ConditionalOnMissingBean(UrlBasedCorsConfigurationSource.class)
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
@@ -27,5 +33,10 @@ public class WebConfig implements WebMvcConfigurer {
         var corsConfigSource = new UrlBasedCorsConfigurationSource();
         corsConfigSource.registerCorsConfiguration("/**", corsConfig);
         return corsConfigSource;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authMemberArgumentResolver);
     }
 }
