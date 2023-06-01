32aa9a8056633df103236cb10babc95440cbe02a
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-06-02 00:58:06 +0900

    feat: mbti domain setup

--- src/main/java/com/service/core/mbti/domain/MbtiMetadata.java
+++ src/main/java/com/service/core/mbti/domain/MbtiMetadata.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
