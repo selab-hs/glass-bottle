e6d22fe512034c6b42ad7bcef984e193bebd9a05
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-06-02 00:58:06 +0900

    feat: mbti domain setup

--- src/main/java/com/service/core/mbti/domain/MbtiQuizRound.java
+++ src/main/java/com/service/core/mbti/domain/MbtiQuizRound.java
@@ -1,6 +1,9 @@
 package com.service.core.mbti.domain;
 
+<<<<<<< HEAD
 import com.service.core.common.domain.BaseEntity;
+=======
+>>>>>>> 2d44878 (feat: mbti domain setup)
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -15,7 +18,11 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "mbti_round")
+<<<<<<< HEAD
 public class MbtiQuizRound extends BaseEntity {
+=======
+public class MbtiQuizRound {
+>>>>>>> 2d44878 (feat: mbti domain setup)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
