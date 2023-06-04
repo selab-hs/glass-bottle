2efb1536253516205398ca831b1e7dc2039eb8b4
HwangHarim <scopan01@naver.com>
2023-06-04 20:12:30 +0900

    :heavy_minus_sign: spring Security 의존성 제거

--- src/main/java/com/service/core/auth/filter/SetAuthenticationStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.service.core.auth.filter;
-
-import org.springframework.security.core.Authentication;
-
-public interface SetAuthenticationStrategy {
-    void set(Authentication authentication);
-}
