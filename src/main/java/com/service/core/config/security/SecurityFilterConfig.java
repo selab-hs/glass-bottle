da3659c0d7f00b435f8ea28cc24276de77d1b244
HwangHarim <scopan01@naver.com>
2023-06-05 16:13:25 +0900

    :fire: 불필요한 filter 제거(LoginCheckFilter)

--- src/main/java/com/service/core/config/security/SecurityFilterConfig.java
+++ src/main/java/com/service/core/config/security/SecurityFilterConfig.java
@@ -1,8 +1,6 @@
 package com.service.core.config.security;
 
 import com.service.core.auth.filter.JwtAuthenticationFilter;
-import com.service.core.auth.filter.LoginCheckFilter;
-import com.service.core.auth.filter.vo.AccessLink;
 import com.service.core.auth.token.TokenProvider;
 import javax.servlet.Filter;
 import lombok.RequiredArgsConstructor;
@@ -15,23 +13,12 @@ import org.springframework.context.annotation.Configuration;
 public class SecurityFilterConfig {
 
     private final TokenProvider tokenProvider;
-    private final AccessLink accessLink;
 
     @Bean
     public FilterRegistrationBean<Filter> JwtAuthenticationCheckFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new JwtAuthenticationFilter(tokenProvider));
         filterFilterRegistrationBean.setOrder(1);
-        filterFilterRegistrationBean.addUrlPatterns("/*");
-
-        return filterFilterRegistrationBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean<Filter> loginCheckFilter() {
-        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-        filterFilterRegistrationBean.setFilter(new LoginCheckFilter(accessLink));
-        filterFilterRegistrationBean.setOrder(2);
         filterFilterRegistrationBean.addUrlPatterns("/*");
 
         return filterFilterRegistrationBean;
