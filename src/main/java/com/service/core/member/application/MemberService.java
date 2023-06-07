8358acfba817395b436355d68a59305eb4212c1d
InJun2 <os2864@naver.com>
2023-06-07 12:37:43 +0900

    * :sparkles: 슬랙 로그 출력

--- src/main/java/com/service/core/member/application/MemberService.java
+++ src/main/java/com/service/core/member/application/MemberService.java
@@ -8,6 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Service
 @RequiredArgsConstructor
 @Lazy
@@ -21,5 +25,19 @@ public class MemberService {
 
     public User viewUser(Long userId){
         return memberRepository.findById(userId).get();
+    }
+
+    public String getYesterdayUsers() {
+        LocalDateTime startDatetime = LocalDateTime.of(LocalDate.now().minusDays(1), LocalTime.of(8,0,1));
+        LocalDateTime endDatetime = LocalDateTime.of(LocalDate.now(), LocalTime.of(8,0,0));
+        StringBuilder message = new StringBuilder();
+
+        var users = memberRepository.findAllByCreatedAtBetween(startDatetime, endDatetime);
+
+        for(User a : users) {
+            message.append("[ CreateTime : " + a.getCreatedAt() + ", userID : " + a.getId() + ", userMBTI : " + a.getMbti() + " ] \n");
+        }
+
+        return message.toString();
     }
 }
--- src/main/java/com/service/core/member/infrastructure/MemberRepository.java
+++ src/main/java/com/service/core/member/infrastructure/MemberRepository.java
@@ -3,6 +3,11 @@ package com.service.core.member.infrastructure;
 import com.service.core.member.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    List<User> findAllByCreatedAtBetween(LocalDateTime startTime, LocalDateTime endTime);
 }
