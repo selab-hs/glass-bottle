5726d7c7e41b7200c2666645c7e096badb22cdf9
HwangHarim <scopan01@naver.com>
2023-06-09 10:40:19 +0900

    :sparkles: mbti qize 기능 구현

--- src/main/java/com/service/core/mbti/domain/MbtiMetadata.java
+++ src/main/java/com/service/core/mbti/domain/MbtiMetadata.java
@@ -5,7 +5,6 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
--- src/main/java/com/service/core/mbti/domain/MbtiQuizRound.java
+++ src/main/java/com/service/core/mbti/domain/MbtiQuizRound.java
@@ -1,9 +1,6 @@
 package com.service.core.mbti.domain;
 
-<<<<<<< HEAD
 import com.service.core.common.domain.BaseEntity;
-=======
->>>>>>> 2d44878 (feat: mbti domain setup)
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -18,11 +15,7 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "mbti_round")
-<<<<<<< HEAD
 public class MbtiQuizRound extends BaseEntity {
-=======
-public class MbtiQuizRound {
->>>>>>> 2d44878 (feat: mbti domain setup)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
