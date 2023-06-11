aadcaa939a997591c3543928f7d0efa89da31288
InJun2 <os2864@naver.com>
2023-06-11 11:40:23 +0900

    * :art: 전날 회원가입 유저가 없을경우 String 반환 및 기능 구분

--- src/main/java/com/service/core/member/application/MemberService.java
+++ src/main/java/com/service/core/member/application/MemberService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -30,12 +31,19 @@ public class MemberService {
     public String getYesterdayJoinUsers() {
         LocalDateTime startDatetime = LocalDateTime.of(LocalDate.now().minusDays(1), LocalTime.of(8,0,1));
         LocalDateTime endDatetime = LocalDateTime.of(LocalDate.now(), LocalTime.of(23,59,0));
+
+        var joinUsers =  memberRepository.findAllByCreatedAtBetween(startDatetime, endDatetime);
+        return usersToString(joinUsers);
+    }
+
+    private String usersToString(List<User> users) {
+        if(users == null || users.isEmpty()) {
+            return "전날 가입한 회원은 존재하지 않습니다.";
+        }
+
         StringBuilder message = new StringBuilder();
-
-        var users = memberRepository.findAllByCreatedAtBetween(startDatetime, endDatetime);
-
-        for(User a : users) {
-            message.append("[ CreateTime : " + a.getCreatedAt() + ", userID : " + a.getId() + ", userMBTI : " + a.getMbti() + " ] \n");
+        for(User user : users) {
+            message.append("[ CreateTime : " + user.getCreatedAt() + ", userID : " + user.getId() + ", userMBTI : " + user.getMbti() + " ] \n");
         }
 
         return message.toString();
