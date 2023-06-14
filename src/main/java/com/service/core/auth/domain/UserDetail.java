97c157530a55291e446a1f584446086af8c26c01
HwangHarim <scopan01@naver.com>
2023-06-14 11:26:11 +0900

    :recycle: member mbti 입력 변경

--- src/main/java/com/service/core/auth/domain/UserDetail.java
+++ src/main/java/com/service/core/auth/domain/UserDetail.java
@@ -1,14 +1,12 @@
 package com.service.core.auth.domain;
 
 import com.service.core.member.domain.User;
-import com.service.core.member.domain.vo.MbtiType;
 import com.service.core.member.domain.vo.RoleType;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-
 
 @Getter
 @Setter
@@ -20,14 +18,14 @@ public class UserDetail {
     private String email;
     private String password;
     private RoleType roleType;
-    private MbtiType mbtiType;
+    private Long mbtiType;
     public static UserDetail create(User user){
         return new UserDetail(
             user.getId(),
             user.getEmail(),
             user.getPassword(),
             RoleType.USER,
-            user.getMbti()
+            user.getMbtiId()
         );
     }
 }
