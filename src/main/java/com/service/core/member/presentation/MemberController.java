8b6302ddf206970cfa0cb0e92273621134583b60
HwangHarim <scopan01@naver.com>
2023-06-16 04:42:29 +0900

    :sparkles: member 탈퇴기능 구현

--- src/main/java/com/service/core/member/presentation/MemberController.java
+++ src/main/java/com/service/core/member/presentation/MemberController.java
@@ -9,6 +9,7 @@ import com.service.core.member.dto.request.CreateMemberRequest;
 import com.service.core.member.dto.response.UserInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,5 +39,11 @@ public class MemberController {
     public ResponseEntity<ResponseDto> createUser(@RequestBody CreateMemberRequest createMemberRequest){
         memberService.createMember(createMemberRequest);
         return ResponseDto.toResponseEntity(ResponseMessage.CREATE_SUCCESS_MEMBER,"회원가입 완료");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ResponseDto> deleteUser(@AuthMember UserInfo user){
+        memberService.deleteMember(user.getId());
+        return ResponseDto.toResponseEntity(ResponseMessage.DELETE_SUCCESS_MEMBER,"회원가입 틸퇴 완료");
     }
 }
