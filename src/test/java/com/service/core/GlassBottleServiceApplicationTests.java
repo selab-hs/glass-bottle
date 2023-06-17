d2d63535d55399ce83f5043c1d13f9db63c96423
InJun2 <os2864@naver.com>
2023-06-17 10:57:27 +0900

    *:bug: test 코드 제거 후 테스트

--- src/test/java/com/service/core/GlassBottleServiceApplicationTests.java
+++ src/test/java/com/service/core/GlassBottleServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GlassBottleServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
