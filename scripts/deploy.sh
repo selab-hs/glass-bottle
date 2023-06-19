b872dacac655cbd69f400819bf952a34a0b8ad67
InJun2 <os2864@naver.com>
2023-06-19 17:08:50 +0900

    * :bug: 코드 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,7 +21,7 @@ else
 fi
 
 echo "> 새 어플리케이션 배포"
-JAR_NAME=$(ls -tr $DEPLOY_PATH/ | grep *.jar | tail -n 1)
+JAR_NAME=$(ls -tr $DEPLOY_PATH | grep *.jar | tail -n 1)
 
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
