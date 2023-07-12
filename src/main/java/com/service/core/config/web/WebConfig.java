70137297b78460978014cf12758599a3a7ffa967
InJun2 <os2864@naver.com>
2023-07-12 23:02:50 +0900

    * :zap: RestTemplate Singleton 으로 사용

--- src/main/java/com/service/core/config/web/WebConfig.java
+++ src/main/java/com/service/core/config/web/WebConfig.java
@@ -1,11 +1,16 @@
 package com.service.core.config.web;
 
 import com.service.core.auth.presentation.AuthMemberArgumentResolver;
+
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -38,5 +43,19 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authMemberArgumentResolver);
+    }
+
+    @Bean
+    public RestTemplate buildRestTemplate(){
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(5000); // 연결시간초과, ms
+        factory.setReadTimeout(5000); // 읽기시간초과, ms
+        HttpClient httpClient = HttpClientBuilder.create()
+                .setMaxConnTotal(100) // 최대 오픈되는 커넥션 수
+                .setMaxConnPerRoute(5) // IP,포트 1쌍에 대해 수행 할 연결 수
+                .build();
+        factory.setHttpClient(httpClient);
+
+        return new RestTemplate(factory);
     }
 }
